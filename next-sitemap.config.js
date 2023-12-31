bab62373936b40d3564b9b51e55b4b9cd5031230
jadsoncerqueira <jadsoncerqueira97@gmail.com>
2023-12-31 18:47:51 -0300

    arrumando sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,17 +5,6 @@ module.exports = {
   robotsTxtOptions: {
     additionalSitemaps: [
       "https://www.loteriascaixaresultados.com/server-sitemap-index.xml",
-      "https://www.loteriascaixaresultados.com/",
-      "https://www.loteriascaixaresultados.com/jogo/maismilionaria",
-      "https://www.loteriascaixaresultados.com/jogo/megasena",
-      "https://www.loteriascaixaresultados.com/jogo/lotofacil",
-      "https://www.loteriascaixaresultados.com/jogo/quina",
-      "https://www.loteriascaixaresultados.com/jogo/lotomania",
-      "https://www.loteriascaixaresultados.com/jogo/timemania",
-      "https://www.loteriascaixaresultados.com/jogo/duplasena",
-      "https://www.loteriascaixaresultados.com/jogo/federal",
-      "https://www.loteriascaixaresultados.com/jogo/diadesorte",
-      "https://www.loteriascaixaresultados.com/jogo/supersete",
     ],
   },
 };
